7d66b59739f52921373d5b5b8abbe3ffab381388
Naginipython <naginipython332@gmail.com>
2023-10-16 15:42:12 -0400

    Finished ex-11

--- ex-10-starting-svelte/src/routes/+page.svelte
+++ ex-10-starting-svelte/src/routes/+page.svelte
@@ -1,33 +1,33 @@
 <script>
     let title = "Ben's hw & test TO-DO list";
-    let unfinished_num = 3;
     let tasks = [
         {
-            id: "text1",
+            id: "text0",
             name: "Japanese wb69-70",
             isChecked: true
         },
         {
-            id: "text2",
+            id: "text1",
             name: "OS P3",
             isChecked: false
         },
         {
-            id: "text3",
+            id: "text2",
             name: "Web ex-10",
             isChecked: true
         },
         {
-            id: "text4",
+            id: "text3",
             name: "Japanese Speaking Test",
             isChecked: false
         },
         {
-            id: "text5",
+            id: "text4",
             name: "Japanese Kanji Quiz",
             isChecked: false
         },
     ];
+    let taskNum = tasks.length;
     function toggleTask(arr, index) {
         console.log("swapped");
         arr[index].isChecked = !arr[index].isChecked;
@@ -38,27 +38,44 @@
         }
         tasks = arr;
     }
+    function addToTasks() {
+        let task = document.getElementById('addToTasks').value;
+        let obj = {
+            id: "text"+taskNum,
+            name: task,
+            isChecked: false
+        };
+        taskNum++;
+        tasks.push(obj);
+        tasks = tasks;
+    }
+
+    function deleteTask(index) {
+        tasks.splice(index, 1);
+        tasks = tasks;
+    }
+
 </script>
 
 <div id="main">
     <h1>{title}</h1>
 
     <br>
-    
-    <input type="checkbox" id="item1" checked={tasks[0].isChecked} on:click={() => {toggleTask(tasks, 0)}}>
-    <label style="text-decoration:line-through" id={tasks[0].id} for="item1">{tasks[0].name}</label><br>
 
-    <input type="checkbox" id="item2" checked={tasks[1].isChecked} on:click={() => {toggleTask(tasks, 1)}}>
-    <label id={tasks[1].id} for="item2">{tasks[1].name}</label><br>
+    <input id="addToTasks" type="text"> <button on:click={addToTasks}>Add to tasks</button>
+    <hr>
 
-    <input type="checkbox" id="item3" checked={tasks[2].isChecked} on:click={() => {toggleTask(tasks, 2)}}>
-    <label style="text-decoration:line-through" id={tasks[2].id} for="item3">{tasks[2].name}</label><br>
+    {#each tasks as item, i}
+        <input class="checkbox" type="checkbox" id="item{i}" checked={item.isChecked} on:click={() => toggleTask(tasks, i)}>
+        {#if item.isChecked}
+            <label style="text-decoration:line-through" id={item.id} for="item{i}">{item.name}</label>
+        {:else}
+            <label id={item.id} for="item{i}">{item.name}</label>
+        {/if}
+        <button on:click={() => deleteTask(i)} class="deleteTask">X</button>
+        <br>
+    {/each}
 
-    <input type="checkbox" id="item4" checked={tasks[3].isChecked} on:click={() => {toggleTask(tasks, 3)}}>
-    <label id={tasks[3].id} for="item4">{tasks[3].name}</label><br>
-
-    <input type="checkbox" id="item5" checked={tasks[4].isChecked} on:click={() => {toggleTask(tasks, 4)}}>
-    <label id={tasks[4].id} for="item5">{tasks[4].name}</label><br>
 </div>
 
 <style>
@@ -73,8 +90,16 @@
     label {
         font-size: x-large;
     }
-    input {
+    .checkbox {
         width: 20px;
         height: 20px;
+    }
+    .deleteTask {
+        width: 20px;
+        height: 20px;
+        border: 1px solid black;
+        float: right;
+        position: relative;
+        top: 8px;
     }
 </style>
